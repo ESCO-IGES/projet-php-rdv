36a869d8d12a2d283afa70525a612c5da6e3d579
yacouba <youedraogo528@gmail.com>
2025-06-19 19:13:14 +0100

    deuxième fichier

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -11,9 +11,36 @@ $rendezvous = $conn->query("SELECT * FROM rendezvous ORDER BY date_heure DESC");
 <!DOCTYPE html>
 <html>
 <head><title>Dashboard</title></head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="assets/style.css">
 <body>
 <h1>Liste des rendez-vous</h1>
-<table border="1">
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="image/logo1.jpg" alt="Logo" style="width:60px;" class="rounded-pill">
+    </a>
+  </div>
+  <div class="container mt-3">
+  
+  <button type="button" class="btn btn-primary">
+    Messages <span class="badge bg-danger">4</span>
+  </button>
+  <button type="button" class="btn btn-danger">
+    Notifications <span class="badge bg-dark">7</span>
+  </button>
+</div>
+</nav>
+
+<div class="container-fluid mt-3">
+  <h3>Page de vérification de rendez-vous</h3>
+  
+</div>
+<div class="container mt-3">
+  <table class="table table-dark table-striped">
     <tr><th>Nom</th><th>Email</th><th>Téléphone</th><th>Objet</th><th>Date/Heure</th><th>Action</th></tr>
     <?php while ($rdv = $rendezvous->fetch_assoc()) { ?>
         <tr>
@@ -26,5 +53,15 @@ $rendezvous = $conn->query("SELECT * FROM rendezvous ORDER BY date_heure DESC");
         </tr>
     <?php } ?>
 </table>
+<div class="container mt-3">
+  
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="image/p1.png" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Yacouba Ouedraogo</h4>
+      <p class="card-text">Administrateur Réseaux et Sécurité</p>
+      <a href="#" class="btn btn-primary">Mon profil</a>
+    </div>
+  </div>
 </body>
 </html>
--- admin/login.php
+++ admin/login.php
@@ -15,14 +15,29 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Connexion Admin</title></head>
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
 <body>
-<h2>Connexion</h2>
+<?php if (!empty($erreur)) echo "<p>$erreur</p>"; ?>
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="image/logo1.jpg" alt="Logo" style="width:60px;" class="rounded-pill">
+    </a>
+  </div>
+</nav> <br>
+
 <form method="POST">
     <input type="text" name="login" placeholder="Login" required><br>
     <input type="password" name="pass" placeholder="Mot de passe" required><br>
     <button type="submit">Se connecter</button>
 </form>
-<?php if (!empty($erreur)) echo "<p>$erreur</p>"; ?>
+
 </body>
 </html>
